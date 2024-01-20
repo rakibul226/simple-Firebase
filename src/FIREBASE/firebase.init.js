65f39eebb3479deefbde73cac5d02dc08ae0b4fa
“rakibul8226” <rakibul8226@gmail.com>
2024-01-20 12:38:14 +0600

    ïnstall and create route

---
+++ src/FIREBASE/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCVAjy6fE5P3Al4OpG5cSdZFGwP5ykDbMw",
+  authDomain: "simple-firebase-61766.firebaseapp.com",
+  projectId: "simple-firebase-61766",
+  storageBucket: "simple-firebase-61766.appspot.com",
+  messagingSenderId: "935191969420",
+  appId: "1:935191969420:web:95fcdf44f72f04e4b385a9",
+  measurementId: "G-DSEYHKECB1"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Home from './Home/Home';
+import Main from './Layout/Main';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+         element:<Home></Home>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
+)
